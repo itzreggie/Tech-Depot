a74ea63d145197898e9debfe67c163349a7153ef
Rejoice Ekong <twicemail5678@gmail.com>
2024-05-12 02:27:22 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -21,19 +21,3 @@ function toggleMenu() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const showMoreButton = document.getElementById('showMoreButton');
-  const pointAContainer = document.getElementById('pointAContainer');
-
-  function togglePointA() {
-    if (pointAContainer.style.display === 'none') {
-      pointAContainer.style.display = 'block';
-      showMoreButton.textContent = 'Show Less';
-    } else {
-      pointAContainer.style.display = 'none';
-      showMoreButton.textContent = 'Show More';
-    }
-  }
-
-  showMoreButton.addEventListener('click', togglePointA);
-});
